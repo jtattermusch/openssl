97769791bb982464568f7407403eacd99ac90300
Jan Tattermusch <jtattermusch@google.com>
2015-04-15 17:17:40 -0700

    build script needs to prepare headers in inc32

--- COPKG/all.bat
+++ COPKG/all.bat
@@ -6,6 +6,17 @@ if "%1"=="noclean" (
 	shift)
 
 setlocal
+rem First do a bit of hacking to make sure we have headers ready in ..\inc32
+cd ..
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+perl Configure no-asm VC-WIN32 || goto :eof
+perl util\mk1mf.pl no-asm VC-WIN32 >ms\nt.mak || goto :eof
+mkdir inc32\openssl
+mkdir tmp32
+nmake -f ms\nt.mak headers || goto :eof
+endlocal
+	
+setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 call :build x64 Release v120 || goto :eof
 call :build x64 Debug v120 || goto :eof
@@ -23,11 +34,11 @@ rem call :build x64 Release v110 || goto :eof
 rem call :build x64 Debug v110 || goto :eof
 rem endlocal
 
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-call :build Win32 Release v110 || goto :eof
-call :build Win32 Debug v110 || goto :eof
-endlocal
+rem setlocal
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+rem call :build Win32 Release v110 || goto :eof
+rem call :build Win32 Debug v110 || goto :eof
+rem endlocal
 
 rem setlocal
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
@@ -48,10 +59,8 @@ goto :clean
 :build
 msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:UsesConfigurationType=dynamic /P:ConfigurationType=DynamicLibrary .\openssl.sln || goto :eof
 msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:UsesConfigurationType=static /P:ConfigurationType=StaticLibrary .\openssl.sln || goto :eof
-REM msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:UsesConfigurationType=ltcg /P:ConfigurationType=ltcg .\openssl.sln || goto :eof
 goto :eof
 
 :clean
-REM rd /s /q v90
 rd /s /q intermediate
 
